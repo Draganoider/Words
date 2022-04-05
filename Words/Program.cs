f13f93af48b9ba2de7c36cf8fa942b66507ebe79
Євген Міщенко <Yevhen.Mishchenko@cs.khpi.edu.ua>
2022-04-05 15:38:11 -0700

    Add project files.

---
+++ Words/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Words
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string text = File.ReadAllText(@"C:\\Users\\evgen\\Desktop\\Sample.txt");
+			char[] arr;
+			int checker = 0;
+
+			int quantity = text.Length;
+			arr = text.ToCharArray(0, quantity);
+
+			for (int i = 0; i < quantity; i++)
+			{
+				if (arr[i] == ' ')
+				{
+					string word = text.Substring(checker, i);
+
+					int wordLenght = word.Length;
+					Regex r = new Regex(word);
+					MatchCollection tx = r.Matches(text);
+					if (tx.Count > 0)
+					{
+						foreach (Match m in tx)
+							Console.WriteLine("Слово в тексте: " + m.Value);
+					}
+					checker += wordLenght + 1;
+					word = "";
+					Console.ReadKey();
+				}
+				
+			}
+
+
+		}
+    }
+}
